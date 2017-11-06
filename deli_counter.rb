2ae539ea3a56962ba82e645f51886595fef90db2
Anthony Fields <janthonyfields@gmail.com>
2017-11-06 22:54:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,11 @@ def line(line_array)
   else
     line_status = "The line is currently: "
     for i in 0..line_array.length - 1 do
-      while i < line_array.length - 1 do
+      if i < line_array.length - 1
         line_status << ("#{i+1}. #{line_array[i]}, ")
+      else
+        line_status << ("#{i+1}. #{line_array[i]}")
       end
-      line_status << ("#{i+1}. #{line_array[i]}")
     end
     line_status
   end
